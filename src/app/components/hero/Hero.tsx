a51b05ee20742163ed08c8cbe08162f1142e16f8
zakaria hadraoui <zikoxo05@gmail.com>
2024-12-06 21:37:46 +0100

    solved center problem and grammar

--- src/app/components/hero/Hero.tsx
+++ src/app/components/hero/Hero.tsx
@@ -10,8 +10,8 @@ function Hero() {
     style={{ background: "url(/assets/images/hero/hero.png)" }}
     className="relative h-[80vh] overflow-hidden py-24 lg:py-32"
   >
-    <div className="container">
-      <div className="max-w-2xl text-center mx-auto">
+    <div className="">
+      <div className="max-w-4xl bg-red-700 text-center mx-auto">
         <h1 className="scroll-m-20 md:text-6xl text-wing-blue text-4xl font-extrabold tracking-tight">
           It's more than <br /> just a trip
         </h1>
