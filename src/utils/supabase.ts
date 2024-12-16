691fcfe382a8e7e5fe1c85246b8f5c59bb38ec60
Ali Farooq <af2747767@gmail.com>
2024-12-16 23:53:42 +0500

    SearchFlight

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,10 +1,10 @@
- import { createClient } from '@supabase/supabase-js';
+//  import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL!;
+// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey);
+// const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 
 
-export default supabase;
+// export default supabase;
